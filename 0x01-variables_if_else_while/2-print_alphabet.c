8fa6a54b3797e752d4f9a7ce3b92b36ec39384be
highbeekay22 <akinbodeibukunoluwa@gmail.com>
2023-07-15 17:59:30 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Retrurn: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			purchar (c);
+			c++;
+		}
+	putchar ("\n");
+	return (0);
+}
