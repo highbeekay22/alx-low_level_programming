2d05d1deb653a4df1760b8d78b3d30ea0ca9d4f2
highbeekay22 <akinbodeibukunoluwa@gmail.com>
2023-07-15 19:03:45 -0700

    gcc -Wall -pedantic -Werror -Wextra -std=gnu89 6-print_numberz.c -o 6-print_numberz

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,19 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - main function
  *
- * Return: Always 0
+ * Return: always 0
  */
 
 int main(void)
 {
 	char i;
 
-	for (i = 'a' : <= 'z' i++)
+	for (i = "a" ; <= "z" i++)
 		putchar(i);
-	for (i = 'A' : <= 'Z' i++)
+	for (i = "A" ; <= "Z" i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
+}
