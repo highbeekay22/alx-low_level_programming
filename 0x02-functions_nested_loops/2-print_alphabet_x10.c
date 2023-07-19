19d0ebdaa6a00ff6bbc5fec278b379f7d0e4ce75
highbeekay22 <akinbodeibukunoluwa@gmail.com>
2023-07-19 01:21:05 -0700

    ./2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,15 +2,14 @@
 
 /**
  * print_alphabet_x10 - make alphabet x10 times
- * Return: voild
+ * Return: void
  */
 
 void print_alphabet_x10(void)
 {
-
 char ch;
 int i;
-i = 0
+i = 0;
 while (i < 10)
 {
 for (ch = 'a'; ch <= 'z'; ch++)
