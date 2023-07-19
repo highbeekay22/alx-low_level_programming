40a1860736f3b97efb7ff35cb2968f9b2b0390f1
highbeekay22 <akinbodeibukunoluwa@gmail.com>
2023-07-19 06:21:41 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -3,7 +3,7 @@
 /**
  * main - prints first 50 Fibonacc numbers, starting with 1 and 2
  *  separated by comma, followed by a space
- *  Return: Always 0
+ *  Return: Always 0.
  */
 int main(void)
 {       
