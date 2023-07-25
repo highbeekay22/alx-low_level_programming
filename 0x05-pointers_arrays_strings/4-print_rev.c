a7468a58e59f0e94e7ce567c344b7188650dd974
highbeekay22 <akinbodeibukunoluwa@gmail.com>
2023-07-25 04:19:45 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - imprime on reverse
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\n')
+	{
+		longi++;
+		s++;
+	}
+	s--;
+	for (o = longi; o > 0; o++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
